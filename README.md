7ce3d8fb4155d4748a8c721004e6c732ffbf801b
Othermusketeer <othermusketeer@gmail.com>
2016-04-07 02:48:57 -0400

    Initial edits. Changes middle mouse emulation to XButton2

--- README.md
+++ README.md
@@ -3,17 +3,21 @@ TrackballScroll
 **Allow scrolling with a trackball without scroll wheel by using a low level mouse hook.**
 
 Changes the behaviour of both X-Buttons (typically buttons 3 and 4) to
-- Scrolling, i.e. vertical mouse wheel events, when an X-Button is pressed and the trackball is moved
-- Middle button click, when an X-Button is pressed and released without trackball movement
+- When the left XButton is held, the cursor is frozen and any vertical movement of the trackball is translated to Scroll Wheel movements.
+- The right XButton acts as a middle mouse button.
+
+###### Credits
+- Original code by Martin Seelge, https://github.com/Seelge/TrackballScroll/
 
 ###### Requirements
 - A trackball or mouse with X-Buttons
 - A Microsoft Windows operating system
 
-This software has been tested with a *Logitech Trackman Marble*(tm) and *Microsoft Windows 8.1* and *7*.
+This software has been tested with a *Logitech Marble Mouse*(tm) and *7*.
 
 ###### Download the latest release
-https://github.com/Seelge/TrackballScroll/releases/latest
+Original version: https://github.com/Seelge/TrackballScroll/releases/latest
+This version: https://github.com/Othermusketeer/TrackballScroll/releases/latest
 
 ###### Run the program
 - Execute `TrackballScroll.exe`, no installation is necessary. A console window is displayed while the program is running, close it to disable the mouse hook.
@@ -21,7 +25,15 @@ https://github.com/Seelge/TrackballScroll/releases/latest
 - The lines scrolled per wheel event are determined by the *Microsoft Windows* mouse wheel settings.
 
 ###### Compile the source code
+- Please at least look at the original author's code before using my edited version
 - Clone the repository from the github page or download the latest source code archive
 - Open the solution with *Microsoft Visual Studio 2013*. If you are using a different version, e.g. *Visual C++ Express*, create a new solution and add the `TrackballScropp.cpp` file.
 - Change the build type to `Release` and `x64`. The program also works in both 64 and 32 bit programs.
 - Build the solution
+
+###### Changelog
+- 2016-04-07	Changed XButton functions. XButton1 activates scroll wheel emulation, and XButton2 emulates middle click
+
+###### Roadmap
+- I may add a tray icon (likely)
+- While I have no need for it, I will consider adding the ability to edit a config file to change how the program functions.
--- TrackballScroll/TrackballScroll.cpp
+++ TrackballScroll/TrackballScroll.cpp
@@ -5,95 +5,160 @@
 // @author Martin Seelge
 // @date 2014-07-12
 ///////////////////////////////////////////////////////////////////////
+//
+// Edits by D'Artagnan Palmer
+// Updated 2016-04-07
+//
+///////////////////////////////////////////////////////////////////////
 #include <windows.h>
 #include <iostream>
+
 const int Y_THRESHOLD = 10;   // threshold in pixels to trigger wheel event
 const DWORD WHEEL_FACTOR = 1; // number of wheel events. The lines scrolled per wheel event are determined by the Microsoft Windows mouse wheel settings.
-enum State {
-	NORMAL = 0, // default state
-	DOWN,       // mouse XButton pressed, no movement
-	SCROLL,     // mouse XButton pressed + moved
-};
+
 HHOOK g_Hook;           // callback function hook, called for every mouse event
-State g_state = NORMAL; // initial state
-POINT g_origin;         // cursor position when entering state DOWN
+POINT g_origin;         // cursor position when entering state x1DOWN
 int g_ycount = 0;       // accumulated vertical movement while in state SCROLL
+
+// If this program is expanded to include configurations, the mouse
+//		states should probably kept with bitflags and bitmasks
+BOOL bXB1 = FALSE;		// Is XButton1 down
+BOOL bXB2 = FALSE;		// Is XButton2 down
+BOOL bScrolling = FALSE;	// Are we in scrolling mode
+
 
 LRESULT CALLBACK LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
 	if (nCode != HC_ACTION)
 		return CallNextHookEx(g_Hook, nCode, wParam, lParam);
+	INPUT inputDat[2];
 	BOOL preventCallNextHookEx = FALSE;
+	// Data about the mouse event
 	const MSLLHOOKSTRUCT *p = reinterpret_cast<const MSLLHOOKSTRUCT*>(lParam);
-	switch (g_state) {
-	case NORMAL:
-		if (wParam == WM_XBUTTONDOWN) { // NORMAL->DOWN: remember position
+
+	// An extended mouse button was pressed
+	if (wParam == WM_XBUTTONDOWN) {
+		// XButton1
+		//WORD w = HIWORD(p->mouseData);
+		if (HIWORD(p->mouseData) & XBUTTON1) {
+#if defined(_DEBUG)
+		std::cout << "XB1 down\n";
+#endif
 			preventCallNextHookEx = TRUE;
-			g_state = DOWN;
+			// Save cursor pos at time of "scroll-click"
 			g_origin = p->pt;
+			bXB1 = TRUE;	// Remember that XButton1 is down
 		}
-		break;
-	case DOWN:
-		if (wParam == WM_XBUTTONUP) { // DOWN->NORMAL: middle button click
+		// XButton2
+		else if (HIWORD(p->mouseData) & XBUTTON2) {
+#if defined(_DEBUG)
+			std::cout << "XB2 down\n";
+#endif
+			// Send a MiddleButton Up message
+			inputDat[0].type = INPUT_MOUSE;
+			inputDat[0].mi.dx = p->pt.x;
+			inputDat[0].mi.dy = p->pt.y;
+			inputDat[0].mi.mouseData = (DWORD)0x0;
+			inputDat[0].mi.dwFlags = MOUSEEVENTF_MIDDLEDOWN; // middle button down
+			inputDat[0].mi.time = (DWORD)0x0;
+			inputDat[0].mi.dwExtraInfo = (ULONG_PTR)NULL;
+			SendInput(1, inputDat, sizeof(INPUT));
+
+			bXB2 = TRUE;	// Remember that XButton2 is down
 			preventCallNextHookEx = TRUE;
-			g_state = NORMAL;
-			INPUT input[2];
-			input[0].type = INPUT_MOUSE;
-			input[0].mi.dx = p->pt.x;
-			input[0].mi.dy = p->pt.y;
-			input[0].mi.mouseData = (DWORD)0x0;
-			input[0].mi.dwFlags = MOUSEEVENTF_MIDDLEDOWN; // middle button down
-			input[0].mi.time = (DWORD)0x0;
-			input[0].mi.dwExtraInfo = (ULONG_PTR)NULL;
-			input[1].type = INPUT_MOUSE;
-			input[1].mi.dx = p->pt.x;
-			input[1].mi.dy = p->pt.y;
-			input[1].mi.mouseData = (DWORD)0x0;
-			input[1].mi.dwFlags = MOUSEEVENTF_MIDDLEUP; // middle button up
-			input[1].mi.time = (DWORD)0x0;
-			input[1].mi.dwExtraInfo = (ULONG_PTR)NULL;
-			SendInput(2, input, sizeof(INPUT));
 		}
-		else if (wParam == WM_MOUSEMOVE) { // DOWN->SCROLL
+	}
+	// An extended mouse button was released
+	else if (wParam == WM_XBUTTONUP) {
+		// XButton1
+		if (HIWORD(p->mouseData) & XBUTTON1) {
+#if defined(_DEBUG)
+			std::cout << "XB1 up\n";
+#endif
 			preventCallNextHookEx = TRUE;
-			g_state = SCROLL;
-			g_ycount = 0;
-			SetCursorPos(g_origin.x, g_origin.y);
+			bScrolling = FALSE;
+			bXB1 = FALSE;	// Forget it. XButton1 is no longer down
 		}
-		break;
-	case SCROLL:
-		if (wParam == WM_XBUTTONUP) { // SCROLL->NORMAL
+		// XButton2
+		else if (HIWORD(p->mouseData) & XBUTTON2) {
+#if defined(_DEBUG)
+			std::cout << "XB2 up\n";
+#endif
+			// Send a MiddleButton Up message
+			inputDat[0].type = INPUT_MOUSE;
+			inputDat[0].mi.dx = p->pt.x;
+			inputDat[0].mi.dy = p->pt.y;
+			inputDat[0].mi.mouseData = (DWORD)0x0;
+			inputDat[0].mi.dwFlags = MOUSEEVENTF_MIDDLEUP; // middle button up
+			inputDat[0].mi.time = (DWORD)0x0;
+			inputDat[0].mi.dwExtraInfo = (ULONG_PTR)NULL;
+			SendInput(1, inputDat, sizeof(INPUT));
+
+			bXB2 = FALSE;	// Forget it. XButton1 is no longer down
 			preventCallNextHookEx = TRUE;
-			g_state = NORMAL;
 		}
-		else if (wParam == WM_MOUSEMOVE) { // SCROLL->SCROLL: wheel event
-			preventCallNextHookEx = TRUE;
-			g_ycount += p->pt.y - g_origin.y;
-			SetCursorPos(g_origin.x, g_origin.y);
-			if (g_ycount < -Y_THRESHOLD || g_ycount > Y_THRESHOLD){
-				DWORD mouseData = (g_ycount > 0 ? -WHEEL_DELTA : +WHEEL_DELTA); // scroll direction
-				g_ycount = 0;
-				INPUT input[WHEEL_FACTOR];
-				for (size_t i = 0; i < WHEEL_FACTOR; ++i) {
-					input[i].type = INPUT_MOUSE;
-					input[i].mi.dx = p->pt.x;
-					input[i].mi.dy = p->pt.y;
-					input[i].mi.mouseData = mouseData;
-					input[i].mi.dwFlags = MOUSEEVENTF_WHEEL; // wheel
-					input[i].mi.time = (DWORD)0x0;
-					input[i].mi.dwExtraInfo = (ULONG_PTR)NULL;
+	}
+	else if (wParam == WM_MOUSEMOVE) {
+		// Is XButton1 being held
+		if (bXB1) {
+			// If we haven't started scrolling...
+			if (!bScrolling) {
+#if defined(_DEBUG)
+				std::cout << "First scroll\n";
+#endif
+				preventCallNextHookEx = TRUE;
+				// Remember that we are now scrolling
+				bScrolling = TRUE;
+				g_ycount = 0;	// Reset our record of verticle movement
+				SetCursorPos(g_origin.x, g_origin.y);
+			}
+			// If we are in the middle of scrolling...
+			else {
+#if defined(_DEBUG)
+				std::cout << " Continuing scroll\n";
+#endif
+				preventCallNextHookEx = TRUE;
+				// Keep track of how much we moved vertically
+				g_ycount += p->pt.y - g_origin.y;
+				// Reset the cursor to its original position
+				SetCursorPos(g_origin.x, g_origin.y);
+				// If we have moved more than the threshold...
+				if (g_ycount < -Y_THRESHOLD || g_ycount > Y_THRESHOLD){
+					DWORD mouseData = (g_ycount > 0 ? -WHEEL_DELTA : +WHEEL_DELTA); // scroll direction
+					g_ycount = 0;
+
+					// Send <WHEEL_FACTOR> number of scroll events
+					INPUT input[WHEEL_FACTOR];
+					for (size_t i = 0; i < WHEEL_FACTOR; ++i) {
+						input[i].type = INPUT_MOUSE;
+						// INFO: Should these be set to g_origin pos instead?
+						input[i].mi.dx = p->pt.x;
+						input[i].mi.dy = p->pt.y;
+						input[i].mi.mouseData = mouseData;
+						input[i].mi.dwFlags = MOUSEEVENTF_WHEEL; // wheel
+						input[i].mi.time = (DWORD)0x0;
+						input[i].mi.dwExtraInfo = (ULONG_PTR)NULL;
+					}
+#if defined(_DEBUG)
+					std::cout << "Sending Scrollwheel event\n";
+#endif
+					SendInput(WHEEL_FACTOR, input, sizeof(INPUT));
 				}
-				SendInput(WHEEL_FACTOR, input, sizeof(INPUT));
 			}
 		}
-		break;
 	}
+
+	// Decide if we should let other programs detect the mouse events
 	return (preventCallNextHookEx ? 1 : CallNextHookEx(g_Hook, nCode, wParam, lParam));
 }
 
 int main() {
+	std::cout << "TrackballScroll v2016.04.07\r\nOriginal Copyright (c) 2014 Martin Seelge, License: The MIT License (MIT)\r\nOriginal Project URL: https://github.com/Seelge/TrackballScroll/\r\n\r\n";
+	std::cout << "Edits by D'Artagnan Palmer on 2016-04-07.\r\nModified Project URL: https://github.com/othermusketeer/TrackballScroll/\r\n\r\n";
+	std::cout << "See README.md for information on how to use this program.\r\n\r\n";
+
 	g_Hook = SetWindowsHookEx(WH_MOUSE_LL, &LowLevelMouseProc, GetModuleHandle(0), 0);
 	if (!g_Hook) return 1; // hook failed
-	std::cout << "TrackballScroll v1.0.1\r\nCopyright (c) 2014 Martin Seelge, License: The MIT License (MIT)\r\n\r\nProject URL: https://github.com/Seelge/TrackballScroll/\r\nSee README.md for information on how to use this program.\r\n\r\nMouse hook registered, ready to scroll.";
+	std::cout << "Mouse hook registered, ready to scroll.\r\nClose this window to terminate scroll emulation.\r\n";
 	MSG message;
 	while (GetMessage(&message, NULL, 0, 0) > 0) // message pump, currently no exit event exists
 		DispatchMessage(&message);
